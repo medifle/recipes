e0f3c3e5e229e5a59c9debe29a8175faccd4bf19
medifle <medifle@gmail.com>
2015-12-15 14:48:38 +0800

    find the  issue

--- browserSync/gulpfile.js
+++ browserSync/gulpfile.js
@@ -1,27 +1,47 @@
-var gulp         = require('gulp');
-var browserSync  = require('browser-sync');
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var $ = gulpLoadPlugins();
-var reload       = browserSync.reload;
+var reload = browserSync.reload;
 
 var src = {
-    scss: 'app/scss/*.scss',
-    css:  'app/css',
-    html: 'app/*.html'
+    scss: '.tmp/scss/*.scss',
+    css: '.tmp/css',
+    html: '.tmp/*.html'
 };
 
 gulp.task('styles', function() {
-  return gulp.src(src.scss)
-    .pipe($.sourcemaps.init())
-    .pipe($.sass({
-      precision: 10
-    }).on('error', $.sass.logError))
-    .pipe($.sourcemaps.write('./'))
-    .pipe(gulp.dest('.tmp/styles'))
+    return gulp.src(src.scss)
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            precision: 10
+        }).on('error', $.sass.logError))
+        .pipe($.sourcemaps.write('./'))
+        .pipe(gulp.dest(src.css))
 
-    // TODO: this works with gulp-if
+    // uncomment one of the following before `gulp serve`
+
+    // this works with gulp-filter
+    // .pipe($.filter('*.css'))
+    // .pipe(reload({stream: true}));
+
+    // this works with gulp-if
     .pipe($.if('*.css', reload({stream: true})));
 
-    // TODO: this works with gulp-filter
+
+    // this `match` work with normal-named dir
+    // however it ignores the dir like `.tmp`
     // .pipe(reload({stream: true, match: '**/*.css'}));
+
+});
+
+gulp.task('serve', ['styles'], function() {
+
+    browserSync({
+        notify: false,
+        server: ".tmp"
+    });
+
+    gulp.watch(src.scss, ['styles']);
+    gulp.watch(src.html).on('change', reload);
 });
